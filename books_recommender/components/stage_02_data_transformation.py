690f3b9b82b3f10fee30ad66e93cd8e488214255
Olga <oola2611@gmail.com>
2026-04-17 10:59:16 +0300

    data_transformation udpated

--- books_recommender/components/stage_02_data_transformation.py
+++ books_recommender/components/stage_02_data_transformation.py
@@ -21,25 +21,25 @@ class DataTransformation:
     def get_data_transformer(self):
         try:
             df = pd.read_csv(self.data_transformation_config.clean_data_file_path)
-            # Lets create a pivot table
+            # Creating a pivot table
             book_pivot = df.pivot_table(columns='user_id', index='title', values= 'rating')
             logging.info(f" Shape of book pivot table: {book_pivot.shape}")
             book_pivot.fillna(0, inplace=True)
 
-            #saving pivot table data
+            # Saving pivot table data
             os.makedirs(self.data_transformation_config.transformed_data_dir, exist_ok=True)
             pickle.dump(book_pivot,open(os.path.join(self.data_transformation_config.transformed_data_dir,"transformed_data.pkl"),'wb'))
             logging.info(f"Saved pivot table data to {self.data_transformation_config.transformed_data_dir}")
 
-            #keeping books name
+            # Keeping books name
             book_names = book_pivot.index
 
-            #saving book_names objects for web app
+            # Saving book_names objects for web app
             os.makedirs(self.data_validation_config.serialized_objects_dir, exist_ok=True)
             pickle.dump(book_names,open(os.path.join(self.data_validation_config.serialized_objects_dir, "book_names.pkl"),'wb'))
             logging.info(f"Saved book_names serialization object to {self.data_validation_config.serialized_objects_dir}")
 
-            #saving book_pivot objects for web app
+            # Saving book_pivot objects for web app
             os.makedirs(self.data_validation_config.serialized_objects_dir, exist_ok=True)
             pickle.dump(book_pivot,open(os.path.join(self.data_validation_config.serialized_objects_dir, "book_pivot.pkl"),'wb'))
             logging.info(f"Saved book_pivot serialization object to {self.data_validation_config.serialized_objects_dir}")
