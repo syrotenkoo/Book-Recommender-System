dbafa1dffa5fb3604382b90b75001ae3a2622904
Olga <oola2611@gmail.com>
2026-04-17 10:58:00 +0300

    data_validation updated

--- books_recommender/components/stage_01_data_validation.py
+++ books_recommender/components/stage_01_data_validation.py
@@ -26,9 +26,8 @@ class DataValidation:
             logging.info(f" Shape of ratings data file: {ratings.shape}")
             logging.info(f" Shape of books data file: {books.shape}")
 
-            #Here Image URL columns is important for the poster. So, we will keep it
             books = books[['ISBN','Book-Title', 'Book-Author', 'Year-Of-Publication', 'Publisher','Image-URL-L']]
-            # Lets remane some wierd columns name in books
+
             books.rename(columns={"Book-Title":'title',
                                 'Book-Author':'author',
                                 "Year-Of-Publication":'year',
@@ -36,25 +35,24 @@ class DataValidation:
                                 "Image-URL-L":"image_url"},inplace=True)
 
             
-            # Lets remane some wierd columns name in ratings
             ratings.rename(columns={"User-ID":'user_id',
                                 'Book-Rating':'rating'},inplace=True)
 
-            # Lets store users who had at least rated more than 200 books
+            # Storing users who had at least rated more than 200 books
             x = ratings['user_id'].value_counts() > 200
             y = x[x].index
             ratings = ratings[ratings['user_id'].isin(y)]
 
-            # Now join ratings with books
+            # Joining ratings with books
             ratings_with_books = ratings.merge(books, on='ISBN')
             number_rating = ratings_with_books.groupby('title')['rating'].count().reset_index()
             number_rating.rename(columns={'rating':'num_of_rating'},inplace=True)
             final_rating = ratings_with_books.merge(number_rating, on='title')
 
-            # Lets take those books which got at least 50 rating of user
+            # Taking those books which got at least 50 rating of user
             final_rating = final_rating[final_rating['num_of_rating'] >= 50]
 
-            # lets drop the duplicates
+            # Dropping the duplicates
             final_rating.drop_duplicates(['user_id','title'],inplace=True)
             logging.info(f" Shape of the final clean dataset: {final_rating.shape}")
                         
@@ -64,7 +62,7 @@ class DataValidation:
             logging.info(f"Saved cleaned data to {self.data_validation_config.clean_data_dir}")
 
 
-            #saving final_rating objects for web app
+            # Saving final_rating objects for web app
             os.makedirs(self.data_validation_config.serialized_objects_dir, exist_ok=True)
             pickle.dump(final_rating,open(os.path.join(self.data_validation_config.serialized_objects_dir, "final_rating.pkl"),'wb'))
             logging.info(f"Saved final_rating serialization object to {self.data_validation_config.serialized_objects_dir}")
